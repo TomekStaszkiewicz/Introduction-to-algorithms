ce1643826e1d0503ecd6eda1bf6b11d726617bb3
tomekstaszkiewicz <staszkiewicztoma@gmail.com>
2021-04-18 17:20:55 +0200

    Check if all elements are in the permutted list

--- Chapter3/permute_in_place_test.go
+++ Chapter3/permute_in_place_test.go
@@ -7,7 +7,7 @@ import (
 
 // PermuteInPlaceTest tests permuteInPlace function
 func TestPermuteInPlace(t *testing.T) {
-	tab := []int{0, 1, 2, 3, 4}
+	tab := []int{0, 1, 2, 3, 4, 5, 1, 2, 9, 8}
 	originalTab := make([]int, len(tab))
 
 	copy(originalTab, tab)
@@ -15,4 +15,22 @@ func TestPermuteInPlace(t *testing.T) {
 	if reflect.DeepEqual(originalTab, tab) {
 		t.Fatalf("Array not permutted!")
 	}
+	allElementsAfterpermutation := true
+
+	for _, value := range originalTab {
+		allElementsAfterpermutation = findInSlice(tab, value)
+	}
+
+	if !allElementsAfterpermutation {
+		t.Fatalf("Array does not contain all the original elements!\nOriginal=%v permutted=%v", originalTab, tab)
+	}
+}
+
+func findInSlice(tab []int, value int) bool {
+	for _, v := range tab {
+		if v == value {
+			return true
+		}
+	}
+	return false
 }
